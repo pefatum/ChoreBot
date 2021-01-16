9fa5eec46b02bb2c31f6247d9db68663abbf60c4
Paul Fatum <p.e.fatum@gmail.com>
2021-01-16 13:33:04 -0500

    chorebot now randomly assigns chores from list of chores to non-bot members of channel

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import discord
 import os
+import random
+from itertools import cycle
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+BOTS_LIST = ['ChoreBot', 'ChoreBot v2']
 
 @client.event
 async def on_ready():
@@ -12,7 +18,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$assignchores'):
+        real_members = [member for member in message.guild.members if member.name not in BOTS_LIST]
+        random.shuffle(real_members)
+        member_cycle = cycle(real_members)
+        chores = open("chores.txt").readlines()
+
+        for chore in chores:
+            member = next(member_cycle)
+            await message.channel.send(member.name + ' please ' + chore)
+
+
 
 client.run(os.getenv('TOKEN'))
